9da48b83bc61fc454f44690abfe30d20cb591dfe
yaman <yaman.dev3@gmail.com>
2024-10-07 20:07:01 +0300

    feature/laravel-health: install laravel health project

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
+use Spatie\Health\Http\Controllers\HealthCheckResultsController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/test', [TestController::class, 'test'])->name('test');
+Route::get('health', HealthCheckResultsController::class);
