50fbceabb523921ab5c5bc46378f5559829fdbf1
yaman <yaman.dev3@gmail.com>
2024-10-07 19:16:13 +0300

    feature/roles-and-permissions: install and configure the permissions package

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,12 +7,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use HasFactory;
     use Notifiable;
     use HasApiTokens;
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
